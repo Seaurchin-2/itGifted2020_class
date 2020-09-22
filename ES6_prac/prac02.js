979c570bdc2e0a14ffb4489be4182e4e5e8c9d5d
Seaurchin-2 <psj22803540@gmail.com>
2020-09-22 19:41:30 +0900

    잠깐 건드린 거

--- ES6_prac/prac02.js
+++ ES6_prac/prac02.js
@@ -18,3 +18,22 @@ let newF = (num1, num2) => {
 }
 newF(2, 8);
 // ----------------------------------------------------
+const obj2 = {
+    host: "psj",
+    server_ip: 896745321
+}
+
+const {a1, b1} = obj2
+console.log(`${a1} || ${b1}`)
+// ----------------------------------------------------
+function addAsync(a, b, callback){
+    setTimeout(() => {
+        callback((a+b)/100);
+    }, 3000);
+}
+  
+console.log('before');
+addAsync(1, 2, function(result){
+    console.log(result);
+});
+console.log('after');

6fecad3160af0f45acb49ca6cd82bea978c923d9
Seaurchin-2 <psj22803540@gmail.com>
2020-09-22 19:00:06 +0900

    es6 연습중 ^^

--- ES6_prac/prac02.js
+++ ES6_prac/prac02.js
@@ -1,1 +1,10 @@
-var a = require("http");
+const obj1 = {
+    number: 8888,
+    id: "sampleobj01",
+    printInfo(){
+        console.log("Object Number: ", this.number);
+        console.log(`Object Id: ${this.id}`);
+    }
+}
+
+obj1.printInfo();
